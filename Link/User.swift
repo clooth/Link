11f3892723d43a751cf30082d872abde9bc67f4c
Nico Hämäläinen <nico@sizeof.io>
2015-04-20 18:22:38 +0300

    Initial User Model placeholder

---
+++ Link/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Link
+//
+//  Created by Nico Hämäläinen on 20/04/15.
+//  Copyright (c) 2015 Lovented Ltd. All rights reserved.
+//
+
+import Foundation
+
+public class User: NSObject {
+    /// Unique identifier
+    public dynamic var id: String = ""
+    /// Publicly displayed username
+    public dynamic var username: String = ""
+}
