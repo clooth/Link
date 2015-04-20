eb8721eb2afb88946d080038780a48031da72207
Nico Hämäläinen <nico@sizeof.io>
2015-04-20 18:20:28 +0300

    Initial commit

---
+++ Link/Link.h
@@ -0,0 +1,19 @@
+//
+//  Link.h
+//  Link
+//
+//  Created by Nico Hämäläinen on 20/04/15.
+//  Copyright (c) 2015 Lovented Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Link.
+FOUNDATION_EXPORT double LinkVersionNumber;
+
+//! Project version string for Link.
+FOUNDATION_EXPORT const unsigned char LinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Link/PublicHeader.h>
+
+
